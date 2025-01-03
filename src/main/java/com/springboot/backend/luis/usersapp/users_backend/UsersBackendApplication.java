ca96b8050f7d7e35d33c1938f3dab74c15028e06
Luis Eduardo Armenta Rea <luiseduardoa.fim@ms.uas.edu.mx>
2025-01-02 22:51:34 -0700

    Ajustes para deploy variables entorno

--- src/main/java/com/springboot/backend/luis/usersapp/users_backend/UsersBackendApplication.java
+++ src/main/java/com/springboot/backend/luis/usersapp/users_backend/UsersBackendApplication.java
@@ -10,4 +10,17 @@ public class UsersBackendApplication {
 		SpringApplication.run(UsersBackendApplication.class, args);
 	}
 
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
